d937fc1949593c92e99561e46744790442cf1f32
Dylski <dylski@dylski>
2018-04-15 14:01:25 +1000

    Final Version

--- PS3DSP/FibonacciIterator.cpp
+++ PS3DSP/FibonacciIterator.cpp
@@ -1,69 +1,57 @@
 #include "FibonacciIterator.h"
-class FibonacciIterator
+#include <iostream>
+using namespace std;
+
+FibonacciIterator::FibonacciIterator(long aMaxN)
 {
-	FibonacciIterator::FibonacciIterator(long aMaxN)
+	fMaxN = aMaxN;
+	fCurrentN = 1;
+	fPrevious = 0;
+	fCurrent = 1;
+}
+const long& FibonacciIterator::operator*() const
+{
+	return fCurrent;
+}
+FibonacciIterator& FibonacciIterator::operator++()
+{
+	int fNext;
+	for (int i = 1; i < fMaxN; i++)
 	{
-		fMaxN = aMaxN;
-		fCurrentN = 1;
-		fPrevious = 0;
-		fCurrent = 0; // these may need to be changed
+		fNext = fCurrent + fPrevious;
+		fPrevious = fCurrent;
+		fCurrent = fNext;
+		cout << fPrevious << endl;
 	}
+	return *this;
+}
 
-	const long & FibonacciIterator::operator*() const
-	{
-		for (int i = 1; i < fMaxN; i++)
-		{
-			fCurrent = fCurrentN + fPrevious;
-			fPrevious = fCurrent;
-			fCurrentN = fCurrent;
-		}
-		return fCurrent;
-	}
-	FibonacciIterator& FibonacciIterator:: operator++()
-	{
-		fCurrentN++;
-	}
-	FibonacciIterator::FibonacciIterator operator++(int)
-	{
-		FibonacciIterator temp = *this;
-		fCurrentN++;
-		return temp;
-	}
-	bool FibonacciIterator::operator==(const FibonacciIterator& aOther) const 
-	{
-		return (fCurrentN == aOther.fCurrentN) &&
-			   (fMaxN == aOther.fMaxN) // this might be wrong fCurrentN == fMaxN
-	}
-	bool FibonacciIterator::operator!=(const FibonacciIterator& aOther)const
-	{
-		return != (*this == aOther); // slide 140
-	}
+FibonacciIterator::FibonacciIterator operator++(int)
+{
+	FibonacciIterator temp = *this;
+	++(*this);
 
-	FibonacciIterator::FibonacciIterator begin() const
-	{
-		FibonacciIterator temp = *this;
-		temp.fMaxN = 1;
-		return temp;
-	}
-	FibonacciIterator::FibonacciIterator end() const
-	{
-		FibonacciIterator temp = *this;
-		temp.fCurrent = -1;
-		return temp; // not sure about this
-	}
-	
+	return temp;
+}
+bool FibonacciIterator::operator==(const FibonacciIterator& aOther) const
+{
+	return fCurrentN == aOther.fCurrentN; // may need correction
+}
+bool FibonacciIterator::operator!=(const FibonacciIterator& aOther) const
+{
+	return !(*this == aOther.fCurrentN);
+}
+FibonacciIterator FibonacciIterator::begin() const
+{
+	FibonacciIterator temp = *this;
+	temp.fCurrentN = 1;
+	++temp;
+	return temp;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-};
+}
+FibonacciIterator FibonacciIterator::end() const
+{
+	FibonacciIterator temp = *this;
+	temp.fCurrentN = fMaxN + 1;
+	return temp;
+}
